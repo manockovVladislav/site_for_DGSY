229cb66ef76f3a47fceba0c414283e6cd43eea36
manockovVladislav <manockov@outlook.com>
2020-01-18 23:38:50 +0300

    исправлен код на новый

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-// $(document).ready(function() {
+$(document).ready(function() {
 
-// 	//E-mail Ajax Send
-// 	$("form").submit(function() { //Change
-// 		var th = $(this);
-// 		$.ajax({
-// 			type: "POST",
-// 			url: "https://glacial-meadow-03048.herokuapp.com/mail.php", //Change
-// 			data: th.serialize()
-// 		}).done(function() {
-// 			alert("Thank you!");
-// 			setTimeout(function() {
-// 				// Done Functions
-// 				th.trigger("reset");
-// 			}, 1000);
-// 		});
-// 		return false;
-// 	});
+	//E-mail Ajax Send
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "https://glacial-meadow-03048.herokuapp.com/mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
 
-// });
+});
