afd13dd1567861d96e3819b0c9456ca0bde5c13e
manockovVladislav <manockov@outlook.com>
2020-01-18 22:36:12 +0300

    исправлен код на новый

--- mail.php
+++ mail.php
@@ -10,6 +10,8 @@ if ( $method === 'POST' ) {
 	$admin_email  = trim($_POST["admin_email"]);
 	$form_subject = trim($_POST["form_subject"]);
 
+	echo $project_name;
+
 	foreach ( $_POST as $key => $value ) {
 		if ( $value != "" && $key != "project_name" && $key != "admin_email" && $key != "form_subject" ) {
 			$message .= "
@@ -26,6 +28,8 @@ if ( $method === 'POST' ) {
 	$project_name = trim($_GET["project_name"]);
 	$admin_email  = trim($_GET["admin_email"]);
 	$form_subject = trim($_GET["form_subject"]);
+
+	echo $admin_email;
 
 	foreach ( $_GET as $key => $value ) {
 		if ( $value != "" && $key != "project_name" && $key != "admin_email" && $key != "form_subject" ) {
