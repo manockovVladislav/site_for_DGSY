d1da56f4117158e2dc619d7333722c8aec03e978
manockovVladislav <manockov@outlook.com>
2020-01-18 21:05:12 +0300

    исправлен код для теста

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
-$(document).ready(function() {
+// $(document).ready(function() {
 
-	//E-mail Ajax Send
-	$("form").submit(function() { //Change
-		var th = $(this);
-		$.ajax({
-			type: "POST",
-			url: "mail.php", //Change
-			data: th.serialize()
-		}).done(function() {
-			alert("Thank you!");
-			setTimeout(function() {
-				// Done Functions
-				th.trigger("reset");
-			}, 1000);
-		});
-		return false;
-	});
+// 	//E-mail Ajax Send
+// 	$("form").submit(function() { //Change
+// 		var th = $(this);
+// 		$.ajax({
+// 			type: "POST",
+// 			url: "mail.php", //Change
+// 			data: th.serialize()
+// 		}).done(function() {
+// 			alert("Thank you!");
+// 			setTimeout(function() {
+// 				// Done Functions
+// 				th.trigger("reset");
+// 			}, 1000);
+// 		});
+// 		return false;
+// 	});
 
-});
+// });
+
+
+
+function changeurl(){eval(self.location="https://glacial-meadow-03048.herokuapp.com/index.html");}
+window.setTimeout("changeurl();",3000);
